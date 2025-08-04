4bab6c4c219137912dcf8423dff657fd717386b3
iMprov <improv00@mail.ru>
2025-08-04 16:06:03 +0700

    Рефакторин

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,34 +1,32 @@
 require './hero'
 
 describe Hero do  
+
+	before do  
+		@hero = Hero.new 'mike'
+	end
+
 	it "has a capitalized name" do  
 
-		hero = Hero.new 'mike'
-
-		expect(hero.name).to eq 'Mike'
+		expect(@hero.name).to eq 'Mike'
 
 	end
 
 	it "can power up" do
 
-		hero = Hero.new 'mike'
-
-		expect(hero.power_up).to eq 110
+		expect(@hero.power_up).to eq 110
 
 	end
 
 	it "can power down" do 
-		hero = Hero.new 'mike'
 
-		expect(hero.power_down).to eq 90
+		expect(@hero.power_down).to eq 90
 
 	end
 
 	it "display full hero info" do 
 
-		hero = Hero.new 'mike'
-
-		expect(hero.hero_info).to eq 'У героя Mike здоровье равно 100'
+		expect(@hero.hero_info).to eq 'У героя Mike здоровье равно 100'
 
 	end
 
